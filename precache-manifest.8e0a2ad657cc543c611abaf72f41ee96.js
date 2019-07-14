f304598a40fdb0ab475b6cb5e67578b34216fa52
Sehbaz <sehbazm@gmail.com>
2019-07-15 00:56:20 +0100

    Updates

---
+++ precache-manifest.8e0a2ad657cc543c611abaf72f41ee96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07721030328475633aad0d54c409fd3c",
+    "url": "/MERN-TODO-APP/index.html"
+  },
+  {
+    "revision": "7d5c16977d1708c06836",
+    "url": "/MERN-TODO-APP/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1120f8817d1bac24ef8c",
+    "url": "/MERN-TODO-APP/static/css/main.0257c661.chunk.css"
+  },
+  {
+    "revision": "7d5c16977d1708c06836",
+    "url": "/MERN-TODO-APP/static/js/2.27dd246d.chunk.js"
+  },
+  {
+    "revision": "1120f8817d1bac24ef8c",
+    "url": "/MERN-TODO-APP/static/js/main.bf224952.chunk.js"
+  },
+  {
+    "revision": "424271b723d5a18271db",
+    "url": "/MERN-TODO-APP/static/js/runtime~main.7c03606c.js"
+  }
+]);
